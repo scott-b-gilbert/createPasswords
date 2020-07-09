2f7a6a3f3ea8cc59c9dd646d72265ec0bf0f5a72
Scott Gilbert <scott.b.gilbert@protonmail.com>
2020-07-09 15:47:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# createPasswords
+A script to create a slightly stronger "words" password than 1Password (or XKCD-password-generator).
+The script, without any arguments, will return around an 18 character password with the following attributes:
+
+1. lowercase letters
+1. random english words
+2. words separated with dashes
+3. one random letter capitalized
+4. one random letter replaced with a number
+
+#TODO
+1. allow different separator
+2. allow more letters capitalized
+3. allow more number replacement
+4. allow longer or set lengths
---
+++ createPass.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+from random import randint
+from xkcdpass import xkcd_password as xp
+
+wordfile = xp.locate_wordfile()
+mywords = xp.generate_wordlist( wordfile = wordfile, min_length=5, max_length=6, valid_chars='.')
+
+password = xp.generate_xkcdpassword( mywords, numwords=4, delimiter='-' )
+print( password )
+
+# still need to randomly replace 1 letter with number and 1 letter with Capital
+
+index1 = -1
+index2 = -1
+# create and replace capital letter
+while( True ):
+    index1 = randint( 0, len(password))
+    print( index1 )
+    if( password[index1] == "-"):
+        continue
+    else:
+        if ( index1 + 1 ) > len( password ):
+            password = password[ 0:index1 ] + password[ index1 ].upper()
+        else:
+            password = password[ 0:index1 ] + password[ index1 ].upper() + password[ index1+1:len(password)+1 ]
+        break
+#create and replace number
+while( True ):
+    index2 = randint( 0, len(password))
+    replaceNum = randint(0, 9)
+    if(index1 == index2 ) or ( password[index2] == "-"):
+        continue
+    else:
+        if ( index2 + 1 ) > len( password ):
+            password = password[ 0:index2 ] + str(replaceNum)
+        else:
+            password = password[ 0:index2 ] + str( replaceNum ) + password[ index2+1:len(password)+1 ]
+        break
+
+print( password )
